dae3774181ed5d15275545260295c65ad1277d96
Eric Nedervold <nedervoldsoftware@gmail.com>
2020-05-17 09:44:19 -0700

    Added a local tasty-discover, since it's not installed on the test machines.

--- test/TidyTests/FileUtilsSpec.hs
+++ test/TidyTests/FileUtilsSpec.hs
@@ -1,15 +1,12 @@
 module TidyTests.FileUtilsSpec where
 
+import System.Directory (getTemporaryDirectory)
 import Test.Tasty
 import Test.Tasty.HUnit
 import TidyTests.FileUtils
-import System.Directory(getTemporaryDirectory)
 
 unit_getProjectDir :: IO ()
 unit_getProjectDir = do
   tmpdir <- getTemporaryDirectory
   res <- getProjectDir tmpdir
-  assertEqual
-    ("should not find project directory above " ++ tmpdir)
-    Nothing
-    res
+  assertEqual ("should not find project directory above " ++ tmpdir) Nothing res
